bb43d3ff4eac804c97087fdb7a088913d7acaeb0
TERmight <denys.mitsel@gmail.com>
2019-03-16 17:03:03 +0200

    protect

---
+++ js/popup-inner.js
@@ -0,0 +1,30 @@
+var buyLink = document.querySelectorAll(".buy");
+var inBasketPopup = document.querySelector(".modal-in-basket");
+var continueButton = inBasketPopup.querySelector(".button-continue");
+var basketClose = inBasketPopup.querySelector(".modal-close");
+
+buyLink.forEach(function(element) {
+  element.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    inBasketPopup.classList.add("modal-show");
+  });
+});
+
+basketClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  inBasketPopup.classList.remove("modal-show");
+});
+
+continueButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  inBasketPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (inBasketPopup.classList.contains("modal-show")) {
+      inBasketPopup.classList.remove("modal-show");
+    }
+  }
+});
